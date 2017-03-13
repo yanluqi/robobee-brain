77651d2ad95d3e1cbe40255347dba476aa74ca9f
nash169 <bernardo.fichera@gmail.com>
2017-03-13 09:56:50 +0100

    Added new header
    New header file encompassing all headers used within the main binary.

---
+++ src/environment.hh
@@ -0,0 +1,32 @@
+/*
+ *  environment.h
+ *
+ *  This file is part of RoboBrain.
+ *  Copyright (C) 2016 Bernardo Fichera
+ *
+ *  RoboBrain is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RoboBrain is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RoboBrain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include "include/robobee.h"
+#include "include/controller.h"
+#include "include/sender.h"
+#include "include/receiver.h"
+#include "include/iomanager.h"
+#include "include/plotter.h"
+
+#endif // ENVIRONMENT_H
