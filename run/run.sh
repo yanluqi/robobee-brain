eb19b37406f54efbe10eceb5b7e914e6e0987101
nash169 <bernardo.fichera@gmail.com>
2017-03-15 23:25:53 +0100

    Add analyze option
    Modify bash script to choose between simulating or analyzing data.

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,12 @@
 cd ../build &&
 make &&
 cp src/main ../run &&
+cp src/analyze ../run &&
 cd ../run &&
-mpirun -np 2 music sim.music
+echo -n "Choose action (1. Simulate, 2. Analyze ) > "
+read num
+if [[ num -eq 1 ]]; then
+  mpirun -np 2 music sim.music
+elif [[ num -eq 2 ]]; then
+  ./analyze
+fi
