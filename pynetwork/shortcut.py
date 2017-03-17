2857d0e1a938256f000b9c6b8e9b69a43c7e2076
nash169 <bernardo.fichera@gmail.com>
2017-03-17 17:49:32 +0100

    Modified structure
    Add LOAD variable to rebuild the neural network starting from a
    previous connections pattern.

--- pynetwork/shortcut.py
+++ pynetwork/shortcut.py
@@ -24,24 +24,35 @@ from tools import *
 from bee_classes import *
 from param_classes import*
 
+LOAD = True
+
 nest.ResetKernel()
 
 box = ParametersBox()
 net = BeeBrain()
 
 net.build(box)
+if LOAD:
+    connToCritic = np.loadtxt('BeeBrain/connToCritic.dat', delimiter=",")
+    connToActor = np.loadtxt('BeeBrain/connToActor.dat', delimiter=",")
+    net.connect(box, connToCritic, connToActor)
+    connCritic = nest.GetConnections(nest.GetNodes(net.cortex)[0], nest.GetNodes(net.critic)[0])
+    connActor = nest.GetConnections(nest.GetNodes(net.cortex)[0], nest.GetNodes(net.actor)[0])
+    weightsCritic = np.array(nest.GetStatus(connCritic, 'weight')) - connToCritic[:,2]
+    weightsActor = np.array(nest.GetStatus(connActor, 'weight')) - connToActor[:,2]
 
-net.connect(box)
-
-connCritic = nest.GetConnections(nest.GetNodes(net.cortex)[0], nest.GetNodes(net.critic)[0])
-connActor = nest.GetConnections(nest.GetNodes(net.cortex)[0], nest.GetNodes(net.actor)[0])
+else:
+    net.connect(box)
+    connCritic = nest.GetConnections(nest.GetNodes(net.cortex)[0], nest.GetNodes(net.critic)[0])
+    connActor = nest.GetConnections(nest.GetNodes(net.cortex)[0], nest.GetNodes(net.actor)[0])
+    weightsCritic = np.array(nest.GetStatus(connCritic, 'weight'))
+    weightsActor = np.array(nest.GetStatus(connActor, 'weight'))
 
 plotNet(net)
-W1 = weightMatrix(connCritic)
+W1 = weightMatrix(nest.GetStatus(connCritic, 'source'), nest.GetStatus(connCritic, 'target'), weightsCritic)
 plot3Dweights(W1, 'Critic Weights','Critic Neurons','Place Cells')
 plot2Dweights(W1, 'Critic Weights','Critic Neurons','Place Cells')
 
-W2 = weightMatrix(connActor)
+W2 = weightMatrix(nest.GetStatus(connActor, 'source'), nest.GetStatus(connActor, 'target'), weightsActor)
 plot3Dweights(W2, 'Actor Weights','Actor Neurons','Place Cells')
 plot2Dweights(W2, 'Actor Weights','Actor Neurons','Place Cells')
-# plt.show()
