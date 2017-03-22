7d8309672206e3e3ad2a6ba48fb95ac8d2e0cc1f
nash169 <bernardo.fichera@gmail.com>
2017-03-22 14:04:24 +0100

    Included new choice
    “Do nothing” option added for just compiling the program.

--- run/run.sh
+++ run/run.sh
@@ -3,10 +3,12 @@ make &&
 cp src/main ../run &&
 cp src/analyze ../run &&
 cd ../run &&
-echo -n "Choose action (1. Simulate, 2. Analyze ) > "
+echo -n "Choose action (1. Simulate, 2. Analyze 3. Do nothing) > "
 read num
 if [[ num -eq 1 ]]; then
   mpirun -np 2 music sim.music
 elif [[ num -eq 2 ]]; then
   ./analyze
+elif [[ num -eq 3 ]]; then
+  echo "Program Compiled"
 fi
